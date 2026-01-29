eb980540bce951e3c608d61f5f625bc0994f2bbe
Alejandro Muzquiz <35324011+Alex232423@users.noreply.github.com>
2026-01-28 21:04:06 -0600

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js')
-    .then(reg => console.log('Registro de APP JANDOS EXITOSO', reg))
-    .catch(err => console.warn('ERROR AL TRATAR DE INSTALAR APP', err))
-}
